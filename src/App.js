78b5c374ab194ac64f9d69f3eadf1e3b31f776f9
Son Nguyen <son18122001@gmail.com>
2024-07-26 13:59:33 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,64 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { Layout, Menu } from 'antd';
-import { ResourceProvider } from './context/ResourceContext';
-import Posts from './components/Posts/Posts';
-import PostDetail from './components/Posts/PostDetail';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Layout, Menu } from "antd";
+import { ResourceProvider } from "./context/ResourceContext";
+import Posts from "./components/Posts/Posts";
 
-import Albums from './components/Albums/Albums';
-import AlbumDetail from './components/Albums/AlbumDetail';
+import Albums from "./components/Albums/Albums";
 
-import Todos from './components/Todos/Todos';
-import TodoDetail from './components/Todos/TodoDetail';
-import Users from './components/Users/Users';
-import UserDetail from './components/Users/UserDetail';
+import Todos from "./components/Todos/Todos";
+
+import Users from "./components/Users/Users";
 
 const { Header, Content, Footer } = Layout;
 
 const App = () => {
-  return (
-    <ResourceProvider>
-      <Router>
-        <Layout className="layout">
-          <Header>
-            <div className="logo" />
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-              <Menu.Item key="1"><Link to="/posts">Posts</Link></Menu.Item>
-              <Menu.Item key="3"><Link to="/albums">Albums</Link></Menu.Item>
-              <Menu.Item key="5"><Link to="/todos">Todos</Link></Menu.Item>
-              <Menu.Item key="6"><Link to="/users">Users</Link></Menu.Item>
-            </Menu>
-          </Header>
-          <Content style={{ padding: '0 50px' }}>
-            <div className="site-layout-content">
-              <Routes>
-                <Route path="/posts" element={<Posts />} />
-                <Route path="/posts/:id" element={<PostDetail />} />
-                <Route path="/albums" element={<Albums />} />
-                <Route path="/albums/:id" element={<AlbumDetail />} />
-                <Route path="/todos" element={<Todos />} />
-                <Route path="/todos/:id" element={<TodoDetail />} />
-                <Route path="/users" element={<Users />} />
-                <Route path="/users/:id" element={<UserDetail />} />
-              </Routes>
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Resource Manager ©2023 Created by You</Footer>
-        </Layout>
-      </Router>
-    </ResourceProvider>
-  );
+    return (
+        <ResourceProvider>
+            <Router>
+                <Layout className="layout">
+                    <Header>
+                        <div className="logo" />
+                        <Menu
+                            theme="dark"
+                            mode="horizontal"
+                            defaultSelectedKeys={["1"]}
+                        >
+                            <Menu.Item key="1">
+                                <Link to="/posts">Posts</Link>
+                            </Menu.Item>
+                            <Menu.Item key="3">
+                                <Link to="/albums">Albums</Link>
+                            </Menu.Item>
+                            <Menu.Item key="5">
+                                <Link to="/todos">Todos</Link>
+                            </Menu.Item>
+                            <Menu.Item key="6">
+                                <Link to="/users">Users</Link>
+                            </Menu.Item>
+                        </Menu>
+                    </Header>
+                    <Content style={{ padding: "0 50px" }}>
+                        <div className="site-layout-content">
+                            <Routes>
+                                <Route path="/posts" element={<Posts />} />
+
+                                <Route path="/albums" element={<Albums />} />
+
+                                <Route path="/todos" element={<Todos />} />
+
+                                <Route path="/users" element={<Users />} />
+                            </Routes>
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: "center" }}>
+                        Resource Manager ©2023 Created by You
+                    </Footer>
+                </Layout>
+            </Router>
+        </ResourceProvider>
+    );
 };
 
 export default App;
